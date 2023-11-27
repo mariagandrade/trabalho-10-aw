6ddca1e78701fe9f3198cbedf3519b3c1a88e701
Maria Andrade <145346053+mariagandrade@users.noreply.github.com>
2023-11-27 13:17:02 +0000

    Add files via upload

---
+++ script.js
@@ -0,0 +1,59 @@
+
+let name = "Maria";
+console.log(name)
+let number = 18;
+console.log(number)
+let number_multiplicado = number * 7;
+console.log(number_multiplicado)
+let amigos = ["carol", "nelson", "mariana"];
+let numbers = [1, 2, 3, 4, 5];
+console.log(amigos[0]);
+// carol
+console.log(amigos[1]);
+// nelson
+console.log(amigos[2]);
+// mariana
+let person = {
+    name: "Maria",
+    age: 18,
+    Student: "verdadeiro"
+  };
+  console.log(person.name);
+  // Maria
+  console.log(person.age);
+  // 18
+  console.log(person.Student);
+  // student
+  let person2 = {
+    name: "Carol",
+    age: 18,
+    Student: "verdadeiro"
+  };
+  console.log(person2.name);
+  // Carol
+  console.log(person2.age);
+  // 18
+  console.log(person2.Student);
+  // student
+  let person3  = {
+    name: "Nelson",
+    age: 18,
+    Student: "verdadeiro"
+  };
+  console.log(person3.name);
+  // Nelson
+  console.log(person3.age);
+  // 18
+  console.log(person3.Student);
+  // studant
+  let person4 = {
+    name: "Mariana",
+    age: 18,
+    Student: "verdadeiro"
+  };
+  console.log(person4.name);
+  // Mariana
+  console.log(person4.age);
+  // 18
+  console.log(person4.Student);
+  // student
